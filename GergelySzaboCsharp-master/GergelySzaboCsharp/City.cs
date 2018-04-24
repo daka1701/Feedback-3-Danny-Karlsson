a1bd67b9de4a3bf43cfaf884b7e9b3a64a69dafb
daka1701 <36623441+daka1701@users.noreply.github.com>
2018-04-24 13:51:35 +0200

    Add files via upload

---
+++ GergelySzaboCsharp-master/GergelySzaboCsharp/City.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace GergelySzaboCsharp
+{
+    class City
+    {
+        private string name;
+        private int nrOfInhabitants;
+        private int averageIncome;
+        private int nrOfTouristsPerYear;
+        private int nrOfAccommodations = 0; //Defaultvärde = 0 eftersom inga accomodations finns lagrade i listan accomodations när vi skapar ett objekt av klassen City!
+        private List<Accommodation> accommodationList; //Länkar till classen Accommodations
+        private double avrageCostPerNight = 0;
+        public City(string name, int nrOfInhabitants, int averageIncome, int nrOfTuristsPerYear)
+        {
+            this.name = name;
+            this.nrOfInhabitants = nrOfInhabitants;
+            this.averageIncome = averageIncome;
+            this.nrOfTouristsPerYear = nrOfTuristsPerYear;
+            accommodationList = new List<Accommodation>();
+            this.PopulatingAccList();
+            this.nrOfAccommodations = 0;
+            this.avrageCostPerNight = 0;
+        }
+         // Metoderna Getters och setters gör att du bestämmer och kontrollerar värdena!
+
+        public string Name { get => name; set => name = value; }
+        public int NrofInhabitants { get => nrOfInhabitants; set => nrOfInhabitants = value; }
+        public int AverageIncome { get => averageIncome; set => averageIncome = value; }
+        public int NrOfTouristsPerYear { get => nrOfTouristsPerYear; set => nrOfTouristsPerYear = value; }
+        public int NrOfAccommodations { get => nrOfAccommodations; set => nrOfAccommodations = value; }
+        public List<Accommodation> AccommodationList { get => accommodationList; set => accommodationList = value; }
+        public void PopulatingAccList()
+        {
+            //initierar SQL connection
+            SqlConnection conn = new SqlConnection(); //Vi skapar ett objekt at typen SqlConnection
+            conn.ConnectionString = "Data Source=TOMBOS2;Initial Catalog=Leo;Integrated Security=True"; //länkenstring till SQL Server och databasen Leo
+            conn.Open(); //Vi öppnar länken mellan C# och SQL
+
+            SqlCommand myQuery = new SqlCommand("SELECT * FROM " + name + ";", conn); // skapar ett SQL query
+            SqlDataReader myReader = myQuery.ExecuteReader(); //nu kan vi läsa data från SQL
+
+            int parsedResult;   //temp variabel för minstay
+            int x = 0; //index för att populera AccommodationList
+            while (myReader.Read())  //Danny: Så länge det finns rader så läser den in dom. Den fyller i alla nedan variablar från sql databasen och lägger den i listan som vi skapade accommodationlist.
+            {
+                int.TryParse(myReader[14].ToString(), out parsedResult);
+                accommodationList.Add(
+                    new Accommodation(
+                        int.Parse(myReader[0].ToString() ?? "0"),//int 
+                        int.Parse(myReader[2].ToString() ?? "0"),//int
+                        myReader[3].ToString() ?? "null",
+                        myReader[6].ToString() ?? "null",
+                        myReader[7].ToString() ?? "null",
+                        int.Parse(myReader[8].ToString() ?? "0"),//int
+                        double.Parse(myReader[9].ToString() ?? "0.0"),
+                        int.Parse(myReader[10].ToString() ?? "0"),//int
+                        int.Parse(myReader[11].ToString() ?? "0"), //int
+                        int.Parse(myReader[13].ToString() ?? "0"),//int
+                        parsedResult,//int
+                        double.Parse(myReader[17].ToString() ?? "0.0"),
+                        double.Parse(myReader[18].ToString() ?? "0.0"),
+                        myReader[16].ToString() ?? "null"
+                    )
+                );
+                avrageCostPerNight = avrageCostPerNight + int.Parse(myReader[13].ToString() ?? "0");
+                x++;
+            }
+            nrOfAccommodations = accommodationList.Count;
+            avrageCostPerNight = avrageCostPerNight/accommodationList.Count;
+            conn.Close();// Danny: Stänger länken mellan C# och SQL 
+        }
+    }
+}//Danny: Lösningen fungerar!
---
+++ GergelySzaboCsharp-master/GergelySzaboCsharp/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GergelySzaboCsharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); //Danny feedback: Initierar den grafiska delen från Form1.Designer.cs samt populerar graferna för första gången
+        }
+    }
+}
+
